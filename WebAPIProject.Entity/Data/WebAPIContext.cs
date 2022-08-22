807172a742e203359251f393a3e0178bf9627468
Cihan Barış Alkaya <95509463+CihanBaris@users.noreply.github.com>
2022-08-22 05:28:42 +0300

    Update WebAPIContext.cs

--- WebAPIProject.Entity/Data/WebAPIContext.cs
+++ WebAPIProject.Entity/Data/WebAPIContext.cs
@@ -12,5 +12,12 @@ namespace WebAPIProject.Entity.Data
         public DbSet<Car> Cars { get; set; }
         public DbSet<Bus> Buses { get; set; }
         public DbSet<Boat> Boats { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>();
+            modelBuilder.Entity<Bus>();
+            modelBuilder.Entity<Boat>();
+
+        }
     }
 }
